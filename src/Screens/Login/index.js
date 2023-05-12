e40a2cee91c745dfe35d69ccdfce246e483cd274
Alan Álvarez <aran.712@gmail.com>
2023-05-11 23:50:46 -0600

    Login design is fixed and completely

--- src/Screens/Login/index.js
+++ src/Screens/Login/index.js
@@ -1,52 +1,66 @@
-import React,{useContext,useState} from 'react'
+import React, { useContext, useState } from "react";
 
-import {LoginContext} from '../../Context/LoginContext'
+import { LoginContext } from "../../Context/LoginContext";
 import Swal from "sweetalert2";
-import { loginApi } from '../../Services/Api'
+import { loginApi } from "../../Services/Api";
+import LogoVetAppBlanco from "../../Assets/Imgs/VetApp-Logo-Blanco.png";
+import LogoVetAppNegro from "../../Assets/Imgs/VetApp-Logo-Negro.png";
+import PethBath from "../../Assets/Icons/pet-bath.svg";
 
-import './index.css'
+import "./index.css";
 
-const url = 'http://localhost:8080/api/users/login';
+const url = "http://localhost:8080/api/users/login";
 
 export const Login = () => {
+  const { login } = useContext(LoginContext);
+  const [form, setForm] = useState({
+    user: "",
+    password: "",
+  });
 
-    const {login} = useContext(LoginContext)
-    const [form,setForm] = useState({
-        user:"",
-        password:""
-    })
+  const handleClick = async () => {
+    login("response.data.token", 1);
+  };
+  return (
+    <div className="login">
+      <section className="login-container">
+        <section className="logo-section">
+          <img
+            media="(max-width: 320px)"
+            className="img-logo-vetapp"
+            src={LogoVetAppNegro}
+          />
+        </section>
+        <section className="login-title">
+          <p>¡Bienvenidos al Sistema!</p>
+          {/* <div></div> */}
+        </section>
 
-    const handleClick = async () => {
-        login("response.data.token",1)
+        <section className="login-inputs">
+          <label>Nombre de Usuario:</label>
+          <input
+            placeholder="Usuario"
+            type="text"
+            value={form.user}
+            onChange={(e) => setForm({ ...form, user: e.target.value })}
+          />
+          <label>Contraseña:</label>
+          <input
+            placeholder="Contraseña"
+            type="password"
+            value={form.password}
+            onChange={(e) => setForm({ ...form, password: e.target.value })}
+          />
+        </section>
 
-    }
-    return (
-        <div className="login">
-
-            <div className="login-container">
-
-                <div className="login-title">
-                    <p>Inicio de sesión</p>
-                    <div></div>
-                </div>
-
-                <div className="login-inputs">
-                    <input 
-                        placeholder="Usuario" 
-                        type="text" 
-                        value={form.user} 
-                        onChange={(e)=>setForm({...form,user:e.target.value})}/>
-                    <input 
-                        placeholder="Contraseña"
-                        type="password" 
-                        value={form.password} 
-                        onChange={(e)=>setForm({...form,password:e.target.value})}/>
-                </div>
-
-                <div className="login-input button" onClick={handleClick}>
-                    <p>Continuar</p>
-                </div>
-            </div>
-        </div>
-    )
-}
+        <button className="login-input button" onClick={handleClick}>
+          <p>ENTRAR</p>
+        </button>
+      </section>
+      <section className="img-background-container">
+        <img className="img-logo-vetapp-blanco" src={LogoVetAppBlanco} />
+        <img src={PethBath} className="img-background-rightside" alt="" />
+      </section>
+    </div>
+  );
+};
